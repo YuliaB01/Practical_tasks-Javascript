c4009c2d0051ab35836b9ec478ad2c89a2aab2ed
Yulia Bilash <starlight05032015@gmail.com>
2017-01-20 14:37:32 +0200

    Task 7

---
+++ Task_7_(19.01.17)/script.js
@@ -0,0 +1,22 @@
+// Напишіть програму, де буде два поля вводу - імя, прізвище. Під ними створіть один `p`, в який буде виводитись інформація в такому форматі:
+// "Привіт, <firstName> <lastName>"
+// 'firstName' - дані з поля вводу імені
+// 'lastName' - дані з поля прізвища
+
+var userFirstName = document.getElementById("firstName");
+var userLastName = document.getElementById("lastName");
+var userFullName = document.getElementById("fullName");
+
+document.getElementById("firstName").addEventListener("keyup", myFunction);
+document.getElementById("lastName").addEventListener("keyup", myFunction);
+
+function myFunction() {
+    userFullName.innerText = "Hello, " + userFirstName.value + " " + userLastName.value + "!";
+}
+
+
+
+// document.getElementById("btn").addEventListener("click", function(){
+//   userFullName.innerText = "Hello, " + userFirstName.value + " " + userLastName.value + "!";
+//
+// });
