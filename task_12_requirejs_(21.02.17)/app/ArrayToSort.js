90f99483606324dcc950c3df286e9706e5cb8142
Yulia Bilash <starlight05032015@gmail.com>
2017-02-23 23:23:40 +0200

    Minor changes in task_12 and finished task_13

--- task_12_requirejs_(21.02.17)/app/ArrayToSort.js
+++ task_12_requirejs_(21.02.17)/app/ArrayToSort.js
@@ -1,4 +1,9 @@
 define([], function() {
-   var numberArray = [65, 25, 83, 23, 1, 4, 12, 76, 45, 254, 6, 135, 987, 8, 10, 44, 67, 98, 114, 453, 4653];
-   return numberArray ;
+    var numberArray = [];
+
+    for (var i = 0, j = 35; i < j; i++) {
+        numberArray.push(Math.round(Math.random() * 500));
+    }
+
+    return numberArray;
 });
